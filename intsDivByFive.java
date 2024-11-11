8b0a678a5ed1d0f8016f8d8fb43b3efc9cec535e
Aayush <ayushms1027@gmail.com>
2024-11-11 10:08:10 +0530

    Done!

--- intsDivByFive.java
+++ intsDivByFive.java
@@ -5,6 +5,14 @@ public class intsDivByFive {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        int n;
+        n = sc.nextInt();
 
+        if(n%5 == 0){
+            System.out.println("Divisible by 5");
+        }
+        else{
+            System.out.println("Not divisible by 5");
+        }
     }
 }
